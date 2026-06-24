0063beaffd904611cb7a4ecddef4de77d9936085
darshanathakare29 <darshana2001thakre@gmail.com>
2026-06-25 00:31:45 +0530

    file updated

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "demo-rg-001"
+  location = "Central India"
+}
